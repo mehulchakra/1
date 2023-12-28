cfbf2ca0ba85d32219b7b5d66bdf239437c9c7fc
mehulchakra <155056085+mehulchakra@users.noreply.github.com>
2023-12-28 15:09:00 +0700

    Update Kursus.java

--- Kursus.java
+++ Kursus.java
@@ -1,14 +1,20 @@
+// Kelas Kursus untuk menyimpan informasi tentang sebuah kursus
 public class Kursus {
+        // Deklarasi variabel
     String name;
     double cost;
     int duration;
-
+    
+  // Konstruktor kelas Kursus
+    // Menginisialisasi nama, biaya, dan durasi kursus
     public Kursus(String name, double cost, int duration){
         this.name = name;
         this.cost = cost;
         this.duration = duration;
     }
-
+    
+    // Metode untuk menampilkan informasi kursus
+    // Menampilkan nama, biaya, dan durasi kursus
     public void getInfo() {
         System.out.println("Informasi Kursus");
         System.out.println("Name: " + name);
