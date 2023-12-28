449893885f356f7bb37953cadacebddeca6fa098
mehulchakra <155056085+mehulchakra@users.noreply.github.com>
2023-12-28 15:14:10 +0700

    Update SistemPendaftaranKursus.java

--- SistemPendaftaranKursus.java
+++ SistemPendaftaranKursus.java
@@ -1,25 +1,28 @@
 import java.util.ArrayList;
 
+// Kelas SistemPendaftaranKursus mengimplementasikan interface Pendaftaran
 public class SistemPendaftaranKursus implements Pendaftaran {
+        // Daftar untuk menyimpan kursus online dan pendaftaran kursus
     private ArrayList<Kursus> daftarKursusOnline;
     private ArrayList<String> listKursusOnline;
-
+    
+    // Konstruktor untuk inisialisasi daftar kursus online dan pendaftaran kursus
     public SistemPendaftaranKursus() {
         this.daftarKursusOnline = new ArrayList<>();
         this.listKursusOnline = new ArrayList<>();
     }
-
+    // Metode untuk menambah kursus ke daftar kursus online
     public void addKursus(Kursus kursus) {
         daftarKursusOnline.add(kursus);
     }
-
+    // Metode untuk menampilkan semua kursus yang tersedia
     public void showKursus() {
         for (Kursus kursus : daftarKursusOnline) {
             kursus.getInfo();
             System.out.println("|===================================|");
         }
     }
-
+    // Metode override untuk mendaftar ke kursus sesuai dengan interface Pendaftaran
     @Override
     public void daftarKursus(String name, int duration) {
         Kursus selectedKursus = null;
@@ -29,13 +32,13 @@ public class SistemPendaftaranKursus implements Pendaftaran {
                 break;
             }
         }
-
+        // Proses pendaftaran jika kursus ditemukan
         if (selectedKursus != null) {
             if (duration > 0 && duration <= selectedKursus.duration) {
                 String daftar = "Kursus: " + name + ", Duration: " + duration;
                 listKursusOnline.add(daftar);
 
-                // selectedKursus.duration -= duration;
+                // selectedKursus.duration -= duration; // Opsional: Mengurangi durasi kursus
 
                 System.out.println("Daftar Successful!");
             } else {
@@ -45,7 +48,7 @@ public class SistemPendaftaranKursus implements Pendaftaran {
             System.out.println("Kursus Not Found.");
         }
     }
-
+    // Metode override untuk menampilkan informasi pendaftaran sesuai dengan interface Pendaftaran
     @Override
     public void tampilkanInfoPendaftaran() {
         for (String kursus : listKursusOnline) {
