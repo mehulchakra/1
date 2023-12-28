bf5db2159f91ea39b9528ef54b5bfb9471eeeab4
mehulchakra <155056085+mehulchakra@users.noreply.github.com>
2023-12-28 15:09:44 +0700

    Update Pendaftaran.java

--- Pendaftaran.java
+++ Pendaftaran.java
@@ -1,4 +1,10 @@
+// Interface Pendaftaran untuk proses pendaftaran kursus
 public interface Pendaftaran {
+     // Metode daftarKursus
+    // Menerima nama kursus dan durasi untuk proses pendaftaran
     void daftarKursus(String name, int duration);
+    
+      // Metode tampilkanInfoPendaftaran
+    // Menampilkan informasi terkait pendaftaran yang telah dilakukan
     void tampilkanInfoPendaftaran();
 }
